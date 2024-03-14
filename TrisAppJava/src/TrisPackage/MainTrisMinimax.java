d8e38cf166224c4e78e6eb1368e89e2e086bde62
RiccardoPiacentini/Riccardo Piacentini <riccardo.piacentini1@galileo.galileicrema.it>
2024-03-14 15:28:41 +0100

    Added Main2 and MinMax

---
+++ TrisAppJava/src/TrisPackage/MainTrisMinimax.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TrisPackage;
+
+/**
+ *
+ * @author ricpi
+ */
+
+public class MainTrisMinimax {
+    public static void main(String[] args) {
+        TrisMinimax trisGame = new TrisMinimax();
+
+        // Simulazione di un gioco del Tris contro l'intelligenza artificiale Minimax
+        trisGame.main(args);
+    }
+}
+
