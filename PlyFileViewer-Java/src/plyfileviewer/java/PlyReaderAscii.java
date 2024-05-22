198d2a0c6691ab0bfe5a5a4cec1dc9215c8e5a6d
Joel Stephan <Joel.stephan.email@gmail.com>
2024-05-22 12:23:27 +0200

    modificato la classe per la lettura file ascii in modo che utilizzi la classe astratta.

--- PlyFileViewer-Java/src/plyfileviewer/java/PlyReaderAscii.java
+++ PlyFileViewer-Java/src/plyfileviewer/java/PlyReaderAscii.java
@@ -3,26 +3,17 @@ package plyfileviewer.java;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class PlyReaderAscii {
+// Classe per leggere file PLY in formato ASCII
+class PlyReaderAscii extends PlyReader {
 
-    private List<String> plyContent;
-
-    public PlyReaderAscii(String filePath) {
-        plyContent = new ArrayList<>();
+    @Override
+    public void readPlyFile(String filePath) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 plyContent.add(line);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
-
-    public List<String> getPlyContent() {
-        return plyContent;
     }
 }
