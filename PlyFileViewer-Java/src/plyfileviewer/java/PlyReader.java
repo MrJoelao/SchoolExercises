a455d00c9d8efdc13e51abcd78641d5bbdfafc0f
Joel Stephan <Joel.stephan.email@gmail.com>
2024-05-22 12:55:35 +0200

    modificato in modo da salvare facce e vertici

--- PlyFileViewer-Java/src/plyfileviewer/java/PlyReader.java
+++ PlyFileViewer-Java/src/plyfileviewer/java/PlyReader.java
@@ -1,24 +1,31 @@
 package plyfileviewer.java;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-// Classe astratta PlyReader
 abstract class PlyReader {
     protected List<String> plyContent;
+    protected List<Vertex> vertices;
+    protected List<Face> faces;
 
     public PlyReader() {
         plyContent = new ArrayList<>();
+        vertices = new ArrayList<>();
+        faces = new ArrayList<>();
     }
 
-    // Metodo astratto che deve essere implementato nelle sottoclassi
     public abstract void readPlyFile(String filePath) throws IOException;
 
-    // Metodo comune per ottenere il contenuto del file
     public List<String> getPlyContent() {
         return plyContent;
+    }
+
+    public List<Vertex> getVertices() {
+        return vertices;
+    }
+
+    public List<Face> getFaces() {
+        return faces;
     }
 }
