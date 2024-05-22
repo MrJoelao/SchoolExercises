508b147cb895137b2d83d0d7db7e943001ceedc6
Joel Stephan <Joel.stephan.email@gmail.com>
2024-05-22 12:02:30 +0200

    Modificata la classe per leggere i file ply in modo che restituisca il contenuto e non venga stampato. (versione binaria)

--- PlyFileViewer-Java/src/plyfileviewer/java/PlyReaderBin.java
+++ PlyFileViewer-Java/src/plyfileviewer/java/PlyReaderBin.java
@@ -3,11 +3,15 @@ package plyfileviewer.java;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlyReaderBin {
-    
+
+    private List<String> plyContent;
 
     public PlyReaderBin(String filePath) {
+        plyContent = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -15,15 +19,12 @@ public class PlyReaderBin {
                 StringBuilder binaryStringBuilder = new StringBuilder();
                 if (line.startsWith("ply") || line.startsWith("format") || line.startsWith("element")
                         || line.startsWith("property") || line.startsWith("end_header")) {
-                    System.out.println(line);
+                    plyContent.add(line);
                 } else if (!line.startsWith("comment")) {
 
                     // Converte ogni byte in una rappresentazione binaria e lo concatena
                     for (byte b : bytes) {
-                        // Utilizza il metodo Integer.toBinaryString per convertire il byte in una
-                        // stringa binaria
-                        // Assicurati di trattare i byte come numeri non firmati per ottenere la
-                        // rappresentazione corretta
+                        // Utilizzo il metodo Integer.toBinaryString per convertire il byte in una stringa binaria
                         String binary = Integer.toBinaryString(b & 0xFF); // Maschera per convertire byte non firmati
                         // Aggiungi zeri iniziali se la rappresentazione binaria è inferiore a 8 bit
                         while (binary.length() < 8) {
@@ -32,11 +33,15 @@ public class PlyReaderBin {
                         // Aggiungi la rappresentazione binaria al StringBuilder
                         binaryStringBuilder.append(binary).append(" ");
                     }
+                    plyContent.add(binaryStringBuilder.toString());
                 }
-                System.out.println(binaryStringBuilder.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<String> getPlyContent() {
+        return plyContent;
     }
 }
