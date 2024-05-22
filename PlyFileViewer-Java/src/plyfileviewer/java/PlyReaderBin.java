20cfa1bdba04d18a18af6f2d0f526fff9b78ad3d
Joel Stephan <Joel.stephan.email@gmail.com>
2024-05-22 12:25:34 +0200

    modificato la classe per la lettura file binari in modo che utilizzi la classe astratta.

--- PlyFileViewer-Java/src/plyfileviewer/java/PlyReaderBin.java
+++ PlyFileViewer-Java/src/plyfileviewer/java/PlyReaderBin.java
@@ -3,15 +3,12 @@ package plyfileviewer.java;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class PlyReaderBin {
+// Classe per leggere file PLY in formato binario
+class PlyReaderBin extends PlyReader {
 
-    private List<String> plyContent;
-
-    public PlyReaderBin(String filePath) {
-        plyContent = new ArrayList<>();
+    @Override
+    public void readPlyFile(String filePath) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -21,27 +18,16 @@ public class PlyReaderBin {
                         || line.startsWith("property") || line.startsWith("end_header")) {
                     plyContent.add(line);
                 } else if (!line.startsWith("comment")) {
-
-                    // Converte ogni byte in una rappresentazione binaria e lo concatena
                     for (byte b : bytes) {
-                        // Utilizzo il metodo Integer.toBinaryString per convertire il byte in una stringa binaria
-                        String binary = Integer.toBinaryString(b & 0xFF); // Maschera per convertire byte non firmati
-                        // Aggiungi zeri iniziali se la rappresentazione binaria è inferiore a 8 bit
+                        String binary = Integer.toBinaryString(b & 0xFF);
                         while (binary.length() < 8) {
                             binary = "0" + binary;
                         }
-                        // Aggiungi la rappresentazione binaria al StringBuilder
                         binaryStringBuilder.append(binary).append(" ");
                     }
                     plyContent.add(binaryStringBuilder.toString());
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
-
-    public List<String> getPlyContent() {
-        return plyContent;
     }
 }
