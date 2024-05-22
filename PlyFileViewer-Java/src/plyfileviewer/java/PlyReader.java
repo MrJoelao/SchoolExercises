07401f5f19c9563108b64cec0b3fe7bf7ca91c87
Joel Stephan <Joel.stephan.email@gmail.com>
2024-05-22 12:21:54 +0200

    modifica classe astratta in modo che sia più strutturata

--- PlyFileViewer-Java/src/plyfileviewer/java/PlyReader.java
+++ PlyFileViewer-Java/src/plyfileviewer/java/PlyReader.java
@@ -1,12 +1,24 @@
 package plyfileviewer.java;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 // Classe astratta PlyReader
-public abstract class PlyReader {
+abstract class PlyReader {
     protected List<String> plyContent;
 
-    // Metodo astratto per ottenere il contenuto del file PLY
-    public abstract List<String> getPlyContent();
+    public PlyReader() {
+        plyContent = new ArrayList<>();
+    }
+
+    // Metodo astratto che deve essere implementato nelle sottoclassi
+    public abstract void readPlyFile(String filePath) throws IOException;
+
+    // Metodo comune per ottenere il contenuto del file
+    public List<String> getPlyContent() {
+        return plyContent;
+    }
 }
