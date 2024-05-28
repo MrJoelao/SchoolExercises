08fb1b768ca0b476d78f1ad32365ecad68028e0a
Joel Stephan <Joel.stephan.email@gmail.com>
2024-05-28 19:09:11 +0200

    Refractor del nome di alcune variabili, sistemazione generale del codice.

--- PlyFileViewer-Java/src/plyfileviewer/java/PlyFileViewerJava.java
+++ PlyFileViewer-Java/src/plyfileviewer/java/PlyFileViewerJava.java
@@ -1,7 +1,6 @@
 package plyfileviewer.java;
 
 import javafx.application.Application;
-import javafx.geometry.Bounds;
 import javafx.geometry.Point3D;
 import javafx.scene.AmbientLight;
 import javafx.scene.Camera;
@@ -15,19 +14,15 @@ import javafx.scene.shape.TriangleMesh;
 import javafx.scene.shape.VertexFormat;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javafx.scene.transform.Rotate;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.transform.Translate;
-import javafx.geometry.Point3D;
 
 /**
  * Classe principale dell'applicazione PlyFileViewerJava.
  * Questa classe estende Application di JavaFX e gestisce la visualizzazione di un file PLY. così pulli :)
  */
-
-
 
 public class PlyFileViewerJava extends Application {
     // Costanti per identificare il tipo di file PLY
@@ -35,10 +30,13 @@ public class PlyFileViewerJava extends Application {
     public static final int BINARY = 1;
     public static final int INVALID = -1;
     public static final int EXCEPTION = -2;
-    Point3D aCenterPoint;
+    private Point3D aCenterPoint;
+    private Point3D CameraPosition;
+    private Camera camera;
+    private boolean debug = false;
 
     // Percorso del file PLY da visualizzare
-    private String filePath = "LowerJawScan.ply"; // LowerJawScan.ply = Binario, airplane.ply = ASCII.
+    private String filePath = "airplane.ply"; // LowerJawScan.ply = Binario, airplane.ply = ASCII.
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -52,12 +50,12 @@ public class PlyFileViewerJava extends Application {
         int plyType = IdentifierPlyType.identifyPlyType(filePath);
         switch (plyType) {
             case ASCII:
-                System.out.println("The file is in ASCII format");
+                if(debug) System.out.println("The file is in ASCII format");
                 plyReader = new PlyReaderAscii();
                 plyReader.readPlyFile(filePath);
                 break;
             case BINARY:
-                System.out.println("The file is in binary format");
+                if(debug) System.out.println("The file is in binary format");
                 plyReader = new PlyReaderBin();
                 plyReader.readPlyFile(filePath);
                 break;
@@ -74,16 +72,13 @@ public class PlyFileViewerJava extends Application {
         // Aggiunta dei vertici al mesh
         for (Vertex vertex : plyReader.getVertices()) {
             mesh.getPoints().addAll((float) vertex.getX(), (float) vertex.getY(), (float) vertex.getZ());
-            System.out.println("Vertex: X=" + vertex.getX() + " Y=" + vertex.getY() + " Z=" + vertex.getZ());
         }
 
         // Aggiunta delle facce al mesh
         for (Face face : plyReader.getFaces()) {
             int[] vertexIndices = face.getVertexIndices();
             mesh.getFaces().addAll(vertexIndices[0], 0, vertexIndices[1], 0, vertexIndices[2], 0);
-            System.out.println("Face: " + vertexIndices[0] + " " + vertexIndices[1] + " " + vertexIndices[2]);
         }
-
 
         mesh.getTexCoords().addAll(0,0);
 
@@ -94,8 +89,9 @@ public class PlyFileViewerJava extends Application {
         root.getChildren().add(meshView);
 
         //Camera
-        camera = new PerspectiveCamera(true);
+        camera = new PerspectiveCamera();
         setCameraPosition(meshView);
+        scene.setCamera(camera);
 
         // Titolo e scena
         primaryStage.setTitle("PlyFileViewer-Java");
@@ -103,81 +99,74 @@ public class PlyFileViewerJava extends Application {
         primaryStage.show();
 
         // Stampa di debug
-        System.out.println("Number of vertices: " + plyReader.getVertices().size());
-        System.out.println("Number of faces: " + plyReader.getFaces().size());
-        plyReader.debugPrintContent();
-        scene.setCamera(camera);
-        /*plyReader.debugPrintVertices();
-        plyReader.debugPrintFaces();*/
-    }
-    public Point3D CameraPosition;
-    public Camera camera;
+        if(debug){
+            System.out.println("Number of vertices: " + plyReader.getVertices().size());
+            System.out.println("Number of faces: " + plyReader.getFaces().size());
+            plyReader.debugPrintContent();
+            plyReader.debugPrintVertices();
+            plyReader.debugPrintFaces();
+        }
 
-    public static void main(String[] args) {
-        launch(args);
     }
 
-    public void setCameraPosition(MeshView m){
-        camera = new PerspectiveCamera();
+    // Imposta la posizione della camera per visualizzare la mesh
+    public void setCameraPosition(MeshView mesh){
+        //camera = new PerspectiveCamera(); // Costruttore già chiamato in start
 
+        // Reset della camera
         camera.getTransforms().clear();
-
         camera.setTranslateX(0);
-
         camera.setTranslateY(0);
-
         camera.setTranslateZ(0);
 
-        Rect3D R = getRect3D(m);
+        // Calcolo del bounding box della mesh
+        Rect3D boundingRect = calculateBoundingBox(mesh);
 
-        Point3D PC = new Point3D((R.MinX+R.MaxX)/2, (R.MinY + R.MaxY) / 2, (R.MinZ + R.MaxZ)/2);
+        // Calcolo del punto centrale del bounding box
+        Point3D centerPoint = new Point3D((boundingRect.MinX + boundingRect.MaxX)/2, (boundingRect.MinY + boundingRect.MaxY) / 2, (boundingRect.MinZ + boundingRect.MaxZ)/2);
+        Point3D cameraPoint = new Point3D(centerPoint.getX(), centerPoint.getY(), boundingRect.MinZ + ((boundingRect.MaxZ-boundingRect.MinZ)* 4));
 
-        Point3D PP = new Point3D(PC.getX(), PC.getY(), R.MinZ + ((R.MaxZ-R.MinZ)* 4));
+        // Impostazione del punto centrale e della posizione della camera
+        aCenterPoint = centerPoint;
+        CameraPosition = cameraPoint;
 
-        aCenterPoint = PC;
+        // Traslazione della camera
+        Translate pivot = new Translate(cameraPoint.getX(), cameraPoint.getY(), cameraPoint.getZ());
+        camera.translateXProperty().set(cameraPoint.getX());
+        camera.translateYProperty().set(cameraPoint.getY());
+        camera.translateZProperty().set(cameraPoint.getZ());
 
-        CameraPosition = PP;
+        // Rotazione della camera
+        Rotate yRotate = new Rotate();
+        yRotate.setAxis(Rotate.Y_AXIS);
+        yRotate.setPivotX(cameraPoint.getX());
+        yRotate.setPivotY(cameraPoint.getY());
+        yRotate.setPivotZ(cameraPoint.getZ());
+        yRotate.setAngle(180);
 
-        Translate pivot = new Translate(PP.getX(),PP.getY(),PP.getZ());
-
-        camera.translateXProperty().set(PP.getX());
-
-        camera.translateYProperty().set(PP.getY());
-
-        camera.translateZProperty().set(PP.getZ());
-
-        Rotate RRR = new Rotate();
-
-        RRR.setAxis(Rotate.Y_AXIS);
-
-        RRR.setPivotX(PP.getX());
-
-        RRR.setPivotY(PP.getY());
-
-        RRR.setPivotZ(PP.getZ());
-
-        RRR.setAngle(180);
-
-        camera.getTransforms().add(RRR);
-
+        // Aggiunta delle trasformazioni alla camera
+        camera.getTransforms().add(yRotate);
         camera.setNearClip(0.125f);
-
         camera.setFarClip(100000f);
 
+        // Aggiunta di una luce ambientale
         AmbientLight ambientLight = new AmbientLight();
-
         ambientLight.setColor(Color.CORAL);
 
         camera.setViewOrder(10);
 
     }
 
-    public Rect3D getRect3D (MeshView m){
+    // Calcola il bounding box di una MeshView
+    public Rect3D calculateBoundingBox(MeshView mesh){
+        // Calcolo del bounding box
+        Rect3D boundingBox = new Rect3D(mesh.getBoundsInParent());
+        aCenterPoint = new Point3D((boundingBox.MinX+boundingBox.MaxX)/2, (boundingBox.MinY+boundingBox.MaxY)/2, (boundingBox.MinZ+boundingBox.MaxZ)/2);
 
-        Rect3D R = new Rect3D(m.getBoundsInParent());
+        return boundingBox;
+    }
 
-        aCenterPoint = new Point3D((R.MinX+R.MaxX)/2, (R.MinY+R.MaxY)/2, (R.MinZ+R.MaxZ)/2);
-
-        return R;
+    public static void main(String[] args) {
+        launch(args);
     }
 }
