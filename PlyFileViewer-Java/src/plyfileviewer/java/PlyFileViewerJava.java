f0941ee4b56e6c9c31bc7c879b483143b4bd1157
Joel <joel.stephan.email@gmail.com>
2024-05-28 14:56:48 +0200

    Sistemazione della camere e della gestione delle facce con la mesh

--- PlyFileViewer-Java/src/plyfileviewer/java/PlyFileViewerJava.java
+++ PlyFileViewer-Java/src/plyfileviewer/java/PlyFileViewerJava.java
@@ -1,6 +1,10 @@
 package plyfileviewer.java;
 
 import javafx.application.Application;
+import javafx.geometry.Bounds;
+import javafx.geometry.Point3D;
+import javafx.scene.AmbientLight;
+import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -8,20 +12,30 @@ import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.DrawMode;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
+import javafx.scene.shape.VertexFormat;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.scene.transform.Rotate;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.transform.Translate;
+import javafx.geometry.Point3D;
 
 /**
  * Classe principale dell'applicazione PlyFileViewerJava.
  * Questa classe estende Application di JavaFX e gestisce la visualizzazione di un file PLY. così pulli :)
  */
+
+
+
 public class PlyFileViewerJava extends Application {
     // Costanti per identificare il tipo di file PLY
     public static final int ASCII = 0;
     public static final int BINARY = 1;
     public static final int INVALID = -1;
     public static final int EXCEPTION = -2;
+    Point3D aCenterPoint;
 
     // Percorso del file PLY da visualizzare
     private String filePath = "LowerJawScan.ply"; // LowerJawScan.ply = Binario, airplane.ply = ASCII.
@@ -56,32 +70,32 @@ public class PlyFileViewerJava extends Application {
         }
 
         // Creazione della mesh da visualizzare
-        TriangleMesh mesh = new TriangleMesh();
+        TriangleMesh mesh = new TriangleMesh(VertexFormat.POINT_TEXCOORD);
         // Aggiunta dei vertici al mesh
         for (Vertex vertex : plyReader.getVertices()) {
             mesh.getPoints().addAll((float) vertex.getX(), (float) vertex.getY(), (float) vertex.getZ());
-
+            System.out.println("Vertex: X=" + vertex.getX() + " Y=" + vertex.getY() + " Z=" + vertex.getZ());
         }
 
         // Aggiunta delle facce al mesh
         for (Face face : plyReader.getFaces()) {
-            for (int index : face.getVertexIndices()) {
-                mesh.getFaces().addAll(index - 1);
-                mesh.getFaces().addAll(0);
-            }
+            int[] vertexIndices = face.getVertexIndices();
+            mesh.getFaces().addAll(vertexIndices[0], 0, vertexIndices[1], 0, vertexIndices[2], 0);
+            System.out.println("Face: " + vertexIndices[0] + " " + vertexIndices[1] + " " + vertexIndices[2]);
         }
 
+
         mesh.getTexCoords().addAll(0,0);
+
         // Creazione della vista della mesh
         MeshView meshView = new MeshView(mesh);
         meshView.setDrawMode(DrawMode.FILL);
         meshView.setMaterial(new PhongMaterial(Color.RED));
-        meshView.setTranslateX(400);
-        meshView.setTranslateY(300);
-        meshView.setTranslateZ(0);
-        meshView.setRotationAxis(Rotate.Y_AXIS);
-        meshView.setRotate(180);
         root.getChildren().add(meshView);
+
+        //Camera
+        camera = new PerspectiveCamera(true);
+        setCameraPosition(meshView);
 
         // Titolo e scena
         primaryStage.setTitle("PlyFileViewer-Java");
@@ -92,11 +106,78 @@ public class PlyFileViewerJava extends Application {
         System.out.println("Number of vertices: " + plyReader.getVertices().size());
         System.out.println("Number of faces: " + plyReader.getFaces().size());
         plyReader.debugPrintContent();
+        scene.setCamera(camera);
         /*plyReader.debugPrintVertices();
         plyReader.debugPrintFaces();*/
     }
+    public Point3D CameraPosition;
+    public Camera camera;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void setCameraPosition(MeshView m){
+        camera = new PerspectiveCamera();
+
+        camera.getTransforms().clear();
+
+        camera.setTranslateX(0);
+
+        camera.setTranslateY(0);
+
+        camera.setTranslateZ(0);
+
+        Rect3D R = getRect3D(m);
+
+        Point3D PC = new Point3D((R.MinX+R.MaxX)/2, (R.MinY + R.MaxY) / 2, (R.MinZ + R.MaxZ)/2);
+
+        Point3D PP = new Point3D(PC.getX(), PC.getY(), R.MinZ + ((R.MaxZ-R.MinZ)* 4));
+
+        aCenterPoint = PC;
+
+        CameraPosition = PP;
+
+        Translate pivot = new Translate(PP.getX(),PP.getY(),PP.getZ());
+
+        camera.translateXProperty().set(PP.getX());
+
+        camera.translateYProperty().set(PP.getY());
+
+        camera.translateZProperty().set(PP.getZ());
+
+        Rotate RRR = new Rotate();
+
+        RRR.setAxis(Rotate.Y_AXIS);
+
+        RRR.setPivotX(PP.getX());
+
+        RRR.setPivotY(PP.getY());
+
+        RRR.setPivotZ(PP.getZ());
+
+        RRR.setAngle(180);
+
+        camera.getTransforms().add(RRR);
+
+        camera.setNearClip(0.125f);
+
+        camera.setFarClip(100000f);
+
+        AmbientLight ambientLight = new AmbientLight();
+
+        ambientLight.setColor(Color.CORAL);
+
+        camera.setViewOrder(10);
+
+    }
+
+    public Rect3D getRect3D (MeshView m){
+
+        Rect3D R = new Rect3D(m.getBoundsInParent());
+
+        aCenterPoint = new Point3D((R.MinX+R.MaxX)/2, (R.MinY+R.MaxY)/2, (R.MinZ+R.MaxZ)/2);
+
+        return R;
     }
 }
