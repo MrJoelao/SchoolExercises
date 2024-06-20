5d38b64c1b47685ded29133bc2001f743c58cb73
Joel Stephan <Joel.stephan.email@gmail.com>
2024-06-20 14:57:14 +0200

    Aggiunta metodi per il salvataggio e lettura dal DB MySQL

--- Paziente.cs
+++ Paziente.cs
@@ -1,46 +1,101 @@
-﻿using System.IO;
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Delta_Dent;
 
 public class Paziente
 {
-    private string nome { get; set; }
-    private string cognome { get; set; }
-    private string luogo_nascita { get; set; }
-    private string provincia { get; set; }
-    private string codice_fiscale { get; set; }
-    private string codice_asl { get; set; }
-    private string categoria { get; set; }
-    private string medico { get; set; }
-    private string professione { get; set; }
-    private string[] indirizzi { get; set; }
-    private DateTime data_nascita { get; set; }
-    private bool estero { get; set; }
-    private bool controllo_validità { get; set; }
-    private bool sesso { get; set; } //true=uomo
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+    public bool Gender { get; set; } // true = male, false = female
+    public string Telephone { get; set; }
+    public string Phone1 { get; set; }
+    public string Phone2 { get; set; }
+    public string CF { get; set; }
+    public string CAsl { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string BirthPlace { get; set; }
+    public string BirthProvince { get; set; }
+    public string Billable { get; set; }
+    public bool Completed { get; set; }
+    public bool Documented { get; set; }
+    
+    public int DoctorID { get; set; }
 
-    public Paziente(string nome, string cognome, string luogoNascita, string provincia, string codiceFiscale, string codiceAsl, string categoria, string medico, string professione, string[] indirizzi, DateTime dataNascita, bool estero, bool controlloValidità, bool sesso)
+    // Costruttore con parametri
+    public Paziente(string firstName, string surname, bool gender, string telephone, string phone1, string phone2,
+        string cf, string cAsl, DateTime birthDate, string birthPlace, string birthProvince, string billable,
+        bool completed, bool documented, int doctorID)
     {
-        this.nome = nome;
-        this.cognome = cognome;
-        luogo_nascita = luogoNascita;
-        this.provincia = provincia;
-        codice_fiscale = codiceFiscale;
-        codice_asl = codiceAsl;
-        this.categoria = categoria;
-        this.medico = medico;
-        this.professione = professione;
-        this.indirizzi = indirizzi;
-        data_nascita = dataNascita;
-        this.estero = estero;
-        controllo_validità = controlloValidità;
-        this.sesso = sesso;
+        FirstName = firstName;
+        Surname = surname;
+        Gender = gender;
+        Telephone = telephone;
+        Phone1 = phone1;
+        Phone2 = phone2;
+        CF = cf;
+        CAsl = cAsl;
+        BirthDate = birthDate;
+        BirthPlace = birthPlace;
+        BirthProvince = birthProvince;
+        Billable = billable;
+        Completed = completed;
+        Documented = documented;
+        DoctorID = doctorID;
     }
 
+    // Costruttore vuoto
     public Paziente()
     {
     }
 
-    public new string ToString()
+    public void SaveInDB()
     {
-        return nome + cognome + data_nascita;
+        // Creo un'istanza di DbManager
+        DbManager db = new DbManager(); // Uso i parametri di default impostati nel costruttore
+
+        // Creo la query da eseguire
+        string query = $"INSERT INTO PATIENT (doctorID, first_name, surname, gender, telephone, phone_1, phone_2, cf, c_asl, birth_date, birth_place, birth_province, billable, completed, documented) VALUES ('{DoctorID}', '{FirstName}', '{Surname}', {Gender}, '{Telephone}', '{Phone1}', '{Phone2}', '{CF}', '{CAsl}', '{BirthDate:yyyy-MM-dd}', '{BirthPlace}', '{BirthProvince}', '{Billable}', {Completed}, {Documented})";
+
+        // Eseguo la query
+        try
+        {
+            db.MakeQuery(query);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Errore durante l'inserimento del paziente: " + e.Message);
+            throw;
+        }
+        
+        db.CloseConnection();
+    }
+
+    public Paziente getFromDBReader(MySqlDataReader reader)
+    {
+        Paziente paziente = new Paziente();
+        paziente.DoctorID = reader.GetInt32("doctorID");
+        paziente.FirstName = reader.GetString("first_name");
+        paziente.Surname = reader.GetString("surname");
+        paziente.Gender = reader.GetBoolean("gender");
+        paziente.Telephone = reader.GetString("telephone");
+        paziente.Phone1 = reader.GetString("phone_1");
+        paziente.Phone2 = reader.GetString("phone_2");
+        paziente.CF = reader.GetString("cf");
+        paziente.CAsl = reader.GetString("c_asl");
+        paziente.BirthDate = reader.GetDateTime("birth_date");
+        paziente.BirthPlace = reader.GetString("birth_place");
+        paziente.BirthProvince = reader.GetString("birth_province");
+        paziente.Billable = reader.GetString("billable");
+        paziente.Completed = reader.GetBoolean("completed");
+        paziente.Documented = reader.GetBoolean("documented");
+        
+        return paziente;
+    }
+
+    // Metodo ToString() per una rappresentazione leggibile dell'oggetto
+    public override string ToString()
+    {
+        return $"{FirstName} {Surname}, Data di nascita: {BirthDate.ToShortDateString()}";
     }
 }
