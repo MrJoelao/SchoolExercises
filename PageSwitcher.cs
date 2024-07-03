04f3f2e923aee09ac99a5e651f5a61a85a1df67e
Luca Spoldi <spoldi.luca06@outlook.it>
2024-07-03 12:47:05 +0200

    aggiunti commenti xml

--- PageSwitcher.cs
+++ PageSwitcher.cs
@@ -3,30 +3,39 @@ using Microsoft.JSInterop;
 
 namespace Delta_Dent;
 
+///<summary>
+///This class is used to switch pages and move files in the application.
+///</summary>
 public class PageSwitcher
 {
     private static NavigationManager navigator;
-
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PageSwitcher"/> class.
+    /// </summary>
+    /// <param name="navigationManager"></param>
     public PageSwitcher(NavigationManager navigationManager)
     {
         navigator = navigationManager;
     }
-
+    
+    /// <summary>
+    /// funtion thas switch the page and copy the file in the tempImg folder
+    /// </summary>
+    /// <param name="id">id of the image</param>
+    /// <param name="path">path where the file will be saved</param>
     [JSInvokable]
     public static Task SwitchPage(string id,String path)
     {
-        Console.Write("\nsto entrando nello switchingg");
         // Get the path to the directory where the image is located
         var imagesDirectoryPath = Path.Combine(path, "Images");
 
         // Get the path to the image file
         var imagePath = Path.Combine(imagesDirectoryPath, id);
         
-        Console.Write("\npath img: " + imagePath);
         // Check if the image file exists
         if (File.Exists(imagePath))
         {   
-            Console.Write("\nl'immagine esiste -> procedo");
             // Get the path to the directory where you want to copy the image
             var tempImagesDirectoryPath = Path.Combine(path, "tempImg");
 
@@ -36,8 +45,7 @@ public class PageSwitcher
             // Get the path to the new image file
             var newImagePath = Path.Combine(tempImagesDirectoryPath, "temp.png");
 
-            // Copy the image file and overwrite if the file already exists
-            Console.Write("sto per copiare "+ imagePath + " in " + newImagePath);
+            // Copy the image file and overwrite if the file already existss
             File.Copy(imagePath, newImagePath, true);
         }
 
