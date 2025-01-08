3943898f0d43aaee9d42d79e25e8d2864c81f582
joelao <joel.stephan.email@gmail.com>
2025-01-08 12:36:46 +0100

    first exercises in php

---
+++ Php/helloWorld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World in PHP</title>
+</head>
+<body>
+    <?php
+        // Messaggio di benvenuto
+        $messaggio = "Hello World!";
+        echo "<h1>$messaggio</h1>";
+    ?>
+</body>
+</html>
+
